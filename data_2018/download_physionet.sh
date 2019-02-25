58129b730321581938dcb12da99f55d9f6c0e13b
Sajad Mousavi <smousavi71@gmail.com>
2019-02-24 18:14:52 -0700

    Update download_physionet.sh

--- data_2018/download_physionet.sh
+++ data_2018/download_physionet.sh
@@ -1,3 +1,4 @@
 
- # this command downloads the whole dataset; 
+# this command downloads the whole dataset
+# wget -r -np --cut-dirs=6 -R "index.html*" https://physionet.org/physiobank/database/sleep-edfx/sleep-cassette/
 wget -nd -r -A edf,hyp https://physionet.org/physiobank/database/sleep-edfx/sleep-cassette/
